87f76237d8e637f481369ffab104d5d1748e6233
IMRD-AL <d.rassomakhin@afterlogic.com>
2024-03-22 10:47:27 +0300

    Add: fault tolerance

--- middleware/routeMiddleware.js
+++ middleware/routeMiddleware.js
@@ -1,15 +1,17 @@
 import { useLocalStorage } from "@/composables/useLocalStorage.js";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  const users = useLocalStorage().getData("allUsers");
-  const router = useRouter();
-  if (users) {
-    const clientExists = users.some((client) => +client.id === +to.params.id);
-    if (!clientExists) {
-      router.replace("/non-existing-route");
-      return false;
+  if (typeof localStorage !== "undefined") {
+    const users = useLocalStorage().getData("allUsers");
+    console.log(users, "users");
+    const router = useRouter();
+    if (users) {
+      const clientExists = users.some((client) => +client.id === +to.params.id);
+      if (!clientExists) {
+        router.replace("/non-existing-route");
+        return false;
+      }
     }
   }
-
   return true;
 });
