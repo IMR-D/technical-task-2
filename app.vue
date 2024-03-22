6aeb204d3608f1f4d2ce480c3ab846ce517cfa6a
IMRD-AL <d.rassomakhin@afterlogic.com>
2024-03-22 03:38:47 +0300

    INIT

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+<script setup></script>
+
+<style>
+body {
+  background-color: #ececec;
+  margin: 0;
+  padding: 0;
+}
+</style>
