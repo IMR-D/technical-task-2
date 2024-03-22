aec21c40111d8322a538fdaa9333bd8a12b6ac4e
IMRD-AL <d.rassomakhin@afterlogic.com>
2024-03-22 03:48:06 +0300

    Update: layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,48 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+defineProps({
+  clients: {
+    type: Array,
+    default: () => [],
+  },
+});
+const isOpen = ref(true);
+</script>
 
 <template>
   <div>
-    <slot />
+    <Sidebar
+      v-model:open="isOpen"
+      :class="{ 'sidebar--open': isOpen, sidebar: true }"
+      :clients
+    />
+    <div :class="{ 'content--compressed': isOpen, content: true }">
+      <slot />
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.content {
+  display: flex;
+  height: 100vh;
+  transition: padding-left 0.3s ease;
+  justify-content: center;
+  align-items: center;
+}
+.content--compressed {
+  padding-left: 300px;
+}
+.sidebar {
+  transform: translateX(-100%);
+  transition: transform 0.3s ease;
+}
+.sidebar--open {
+  transform: translateX(0);
+}
+@media screen and (max-width: 769px) {
+  .content--compressed {
+    padding-left: 0;
+  }
+}
+</style>
